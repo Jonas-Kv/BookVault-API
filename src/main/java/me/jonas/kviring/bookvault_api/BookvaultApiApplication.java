513897a6e01406823c0aee031c85ad3c62e33600
Jonas Kviring <kviring456@gmail.com>
2026-04-02 00:55:46 +0200

    first commit

---
+++ src/main/java/me/jonas/kviring/bookvault_api/BookvaultApiApplication.java
@@ -0,0 +1,13 @@
+package me.jonas.kviring.bookvault_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookvaultApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookvaultApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/jonas/kviring/bookvault_api/BookvaultApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jonas.kviring.bookvault_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookvaultApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
